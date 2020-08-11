f314d3f6d05b176f1f6944f372fb244eeb6729f7
david-shams-bp <david.shams.innova@gmail.com>
2020-08-11 14:29:14 -0500

    Sheppartd added to existing angular application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+// import { ShepherdService } from 'angular-shepherd';
+
 
 @Component({
   selector: 'app-root',
@@ -8,3 +10,23 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'shepherd-multi-lingual';
 }
+
+// import { steps as defaultSteps, defaultStepOptions} from './data';
+
+// @Component({
+//   selector: 'shepherd',
+//   templateUrl: './shepherd.component.html',
+//   styleUrls: ['./shepherd.component.css']
+// })
+// export class ShepherdComponent implements AfterViewInit {
+
+//   constructor(private shepherdService: ShepherdService) { }
+
+//   ngAfterViewInit() {
+//     this.shepherdService.defaultStepOptions = defaultStepOptions;
+//     this.shepherdService.modal = true;
+//     this.shepherdService.confirmCancel = false;
+//     this.shepherdService.addSteps(defaultSteps);
+//     this.shepherdService.start();
+//   }
+// }
