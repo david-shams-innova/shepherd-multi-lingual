522432c5de0abc6aa5c5efa4eb4bbab5c8c9d832
david-shams-bp <david.shams.innova@gmail.com>
2020-08-11 20:30:28 -0500

    import bootstrap styles working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component, AfterViewInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css',  "../../node_modules/bootstrap/dist/css/bootstrap.min.css"]
+  styleUrls: ['./app.component.css']
   // styleUrls: ['./app.component.css']
 })
 export class AppComponent {
